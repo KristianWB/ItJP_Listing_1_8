47aabe392229b76ceeaca213d9699b4707df2ec9
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-25 13:40:56 +0200

    Initial commit

---
+++ src/AreaAndPerimeterOfACircle.java
@@ -0,0 +1,13 @@
+public class AreaAndPerimeterOfACircle {
+    public static void main(String[] args)   {
+
+        /*
+         we need to built two print commands that displays the area and the perimeter of a circle with a predetermined radius
+          */
+            // First the perimeter
+        System.out.println(String.format("%.2f", (2 * 6.5 * Math.PI)));
+
+            // Second the area
+        System.out.println(String.format("%.2f",(6.5 * 6.5 * Math.PI)));
+    }
+}
